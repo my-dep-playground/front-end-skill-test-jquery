08c3c0193a8dfa85ff3594b4ca3189584f10d467
Poorni Amaratunga <poorniamarathunga1997@gmail.com>
2020-11-20 01:06:17 +0530

    add validation to address

--- js/customer-ctrl.js
+++ js/customer-ctrl.js
@@ -43,25 +43,71 @@ function init(){
 
 /*===============================================================================
  * Event Handlers and Timers
- *===============================================================================*/
+ *================================================================= ==============*/
+
+/*txtId.on("change",handleInput);
+txtName.on("change",handleInput);
+txtAddress.on("click",handleInput);*/
+
 
 $("#btn-save").click(function (){
-    $("table tbody").append('<tr>\n' +
-        '                        <td>'+$("#txt-id").val()+'</td>\n' +
-        '                        <td>'+$("#txt-name").val()+'</td>\n' +
-        '                        <td>'+$("#txt-address").val()+'</td>\n' +
-        '                        <td><button type="button">Delete</button></td>\n' +
-        '                    </tr>');
-    showOrHideTFoot();
-    $("tbody button").click(function (){
-        $(this).parents("tr").remove();
+    if(validate()) {
+        $("table tbody").append('<tr>\n' +
+            '                        <td>' + $("#txt-id").val() + '</td>\n' +
+            '                        <td>' + $("#txt-name").val() + '</td>\n' +
+            '                        <td>' + $("#txt-address").val() + '</td>\n' +
+            '                        <td><div id="trash-icon"></div></td>\n' +
+            '                    </tr>');
         showOrHideTFoot();
-    })
+        attachEventListner();
+    }else {
+        return;
+    }
+
 });
 
 /*===============================================================================
  * Functions
  *===============================================================================*/
+
+function handleInput(event){
+    this.removeClass('is-invalid');
+}
+
+function attachEventListner(){
+        $("#trash-icon").click(function () {
+            if (confirm("Are you sure whether you want to delete this customer?")) {
+                $(this).parents("tr").remove();
+            }
+            showOrHideTFoot();
+        });
+}
 function showOrHideTFoot(){
     $("table tbody tr").length > 0 ? $("table tfoot").addClass("hide") : $("table tfoot").removeClass("hide");
+}
+
+function validate(){
+    var regExp = null;
+    var validated = true;
+
+    txtId.removeClass('is-invalid');
+    txtName.removeClass('is-invalid');
+    txtAddress.removeClass('is-invalid');
+
+    if($.trim(txtAddress.val()).length < 3){
+        txtAddress.addClass('is-invalid');
+        txtAddress.select();
+        validated = false;
+    }
+    var name = $("#txt-name").val();
+    var reg = /^[A-Za-z][A-Za-z .]{3,}$/;
+    if(!name.text(reg)){
+        alert("hello");
+    }
+ /*   if (!txtName.val().text().match(/^[A-Za-z][A-Za-z .]{3,}$/)){
+        txtName.addClass('is-invalid');
+        txtName.select();
+        validated = false;
+    }*/
+    return validated;
 }
